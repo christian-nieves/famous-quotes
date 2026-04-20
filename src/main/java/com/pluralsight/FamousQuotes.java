d8a84b2b5756912a2c678be5c244fa679290a0f2
christian-nieves <thechristiannieves@gmail.com>
2026-04-20 10:36:16 -0400

    Second part complete displaying error if number doesn't exist

--- src/main/java/com/pluralsight/FamousQuotes.java
+++ src/main/java/com/pluralsight/FamousQuotes.java
@@ -7,23 +7,27 @@ public class FamousQuotes {
 
         Scanner scanner = new Scanner(System.in);
 
-        String quotes[] = new String[10];
+        try {
+            String quotes[] = new String[10];
 
-        quotes[0] = ("What happens in the dark, always comes to light.");
-        quotes[1] = ("Who am I to judge when I myself walk as an imperfect man.");
-        quotes[2] = ("What we think, we become.");
-        quotes[3] = ("The purpose of life is to be happy.");
-        quotes[4] = ("You have one life to live, enjoy it.");
-        quotes[5] = ("Dream big and dare to fail.");
-        quotes[6] = ("The best way out is always through.");
-        quotes[7] = ("Happiness depends on ourselves.");
-        quotes[8] = ("The only way to do great work is to love what you do.");
-        quotes[9] = ("It does not matter how slowly you go as long as you do not stop.");
+            quotes[0] = ("What happens in the dark, always comes to light.");
+            quotes[1] = ("Who am I to judge when I myself walk as an imperfect man.");
+            quotes[2] = ("What we think, we become.");
+            quotes[3] = ("The purpose of life is to be happy.");
+            quotes[4] = ("You have one life to live, enjoy it.");
+            quotes[5] = ("Dream big and dare to fail.");
+            quotes[6] = ("The best way out is always through.");
+            quotes[7] = ("Happiness depends on ourselves.");
+            quotes[8] = ("The only way to do great work is to love what you do.");
+            quotes[9] = ("It does not matter how slowly you go as long as you do not stop.");
 
-        System.out.println("Select a quote(1-10): ");
-        int quoteSelect = scanner.nextInt();
+            System.out.println("Select a quote(1-10): ");
+            int quoteSelect = scanner.nextInt();
 
-        System.out.println(quotes[quoteSelect - 1]);
+            System.out.println(quotes[quoteSelect - 1]);
+        } catch (Exception e) {
+            System.out.println("Sorry the number you selected does not exist. Please try again.");
+        }
 
 
     }
